3cc63a056c998c65154d26e9bc5895ad891d37f0
Peter Sampson <peter.sampson010@gmail.com>
2019-11-13 13:01:13 +0000

    Gungan Bongo Submarine

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -3,6 +3,10 @@ class RocketsController < ApplicationController
     def slug
         name = params[:slug].gsub(" ", "-").downcase
         @rocket = Rocket.find_by_slug(name)
-        render "/rockets/show"
+        if @rocket 
+            render "/rockets/show"
+        else 
+            redirect_to trips_path
+        end 
     end 
 end
