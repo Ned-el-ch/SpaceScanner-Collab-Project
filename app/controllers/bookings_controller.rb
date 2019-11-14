75a4944138d138f95167e6f927e283773b0d4259
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-14 09:31:47 +0000

    added search functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,15 +2,27 @@ class BookingsController < ApplicationController
 
     def index
 
-        @trips = []
+        # @trips = []
 
-        @trips << Trip.find_by(origin_id: params[:origin_id], destination_id: params[:destination_id])
+        @valid_trips = Trip.where(origin_id: params[:origin_id], destination_id: params[:destination_id])
         # @trips = {
         #     origin: Planet.find(params[:origin_id]), 
         #     destination: Planet.find(params[:destination_id]),
         #     date: "#{params[:date_takeoff_date_1i]}-#{params[:date_takeoff_date_2i]}-#{params[:date_takeoff_date_3i]}"
         # }
-        byebug
+
+        @trips = []
+        @valid_trips.each_with_index do |trip, index|
+            @trips << {
+                origin: Planet.find(trip.origin_id), 
+                destination: Planet.find(trip.destination_id), 
+                rocket: trip.rocket, 
+                carrier: trip.carrier, 
+                distance: Planet.find(trip.origin_id).distance_from_earth + Planet.find(trip.destination_id).distance_from_earth, 
+                duration: Planet.find(trip.destination_id).distance_from_earth/trip.rocket.speed}
+            
+        end
+        # byebug
         render "/trips/_trips"
     end 
 
